28cc6ec0f25ef7523d8219a603968d91f5a64852
bikash.k <bikash.k@LP-5CD6495TH7.HCLC.CORP.HCL.IN>
2018-10-25 17:05:34 +0530

    new addition changes

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,11 +3,14 @@
  * 
  */
 $(document).ready(function(){
-	$('.table .eBtn').on('click',function(event){
-		//alert("called here");
+	$('.nBtn, .table .eBtn').on('click',function(event){		
 		event.preventDefault();
 		var href=$(this).attr('href');
 		console.log(href);
+		var text=$(this).text();
+		//alert("called here:"+text);		
+		if(text=="Edit"){
+			console.log(text);
 		$.get(href,function(company,status){
 			//console.log(status+":"+company);
 			$('.companyForm #companyId').val(company.companyId);
@@ -15,6 +18,14 @@ $(document).ready(function(){
 			$('.companyForm #description').val(company.description);
 			$('.companyForm #createdBy').val(company.createdBy);
 		});
+		}
+		else{
+			console.log(text);
+			$('.companyForm #companyId').prop( "disabled", true );
+			$('.companyForm #companyName').val('');
+			$('.companyForm #description').val('');
+			$('.companyForm #createdBy').val('');
+		}
 		$('.companyForm #companyModal').modal();
 	});
 });
